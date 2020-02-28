38b0b4f1cc4274059fb711a8fff724802b0f0a21
Christian de Barrio <christian.debarrio@cabify.com>
2020-02-28 12:48:02 +0100

    increase the rows to 1000

--- Checklist/ChecklistViewController.swift
+++ Checklist/ChecklistViewController.swift
@@ -16,7 +16,7 @@ class ChecklistViewController: UITableViewController {
     }
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 1
+    return 1000
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
