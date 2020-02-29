e62e4f1e170c4cc4ae3c84a4f000e96c9f0efe00
Christian de Barrio <christian.debarrio@cabify.com>
2020-02-29 13:28:19 +0100

    enable add button behaviour

--- Checklist/AddItemTableViewController.swift
+++ Checklist/AddItemTableViewController.swift
@@ -10,19 +10,56 @@ import UIKit
 
 class AddItemTableViewController: UITableViewController {
   
+  @IBOutlet weak var textfield: UITextField!
+  
+  @IBOutlet weak var addBarButton: UIBarButtonItem!
+  
+  @IBOutlet weak var cancelBarButton: UIBarButtonItem!
+  
   @IBAction func cancel(_ sender: Any) {
     navigationController?.popViewController(animated: true)
   }
   
   @IBAction func done(_ sender: Any) {
     navigationController?.popViewController(animated: true)
+//    print("Contents \(String(describing: textfield.text))")
   }
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
     navigationItem.largeTitleDisplayMode = .never
+    textfield.delegate = self
   }
   
-  @IBOutlet weak var textField: UITextField!
+  override func viewWillAppear(_ animated: Bool) {
+    textfield.becomeFirstResponder()
+  }
+  
+  override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+    return nil
+  }
+  
+}
+
+extension AddItemTableViewController: UITextFieldDelegate {
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    textfield.resignFirstResponder()
+    return false
+  }
+  
+  func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    guard let oldText = textfield.text,
+      let stringRange = Range(range, in: oldText) else {
+        return false
+    }
+    
+    let newText = oldText.replacingCharacters(in: stringRange, with: string)
+    if newText.isEmpty {
+      addBarButton.isEnabled = false
+    } else {
+      addBarButton.isEnabled = true
+    }
+    return true
+  }
 }
